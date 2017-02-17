6d023a0daf901407e5ddfeb6c7f80bde566ac2ac
Erwann de Dieuleveult <ededieuleveult@gmail.com>
2017-02-17 01:38:33 +0100

    done some style on index, show and new cocktails views, update a logo.png for navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ URL = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 list = open(URL)
 Ingredient.destroy_all
 
-JSON.parse(list.read)["drinks"].each do |h|
-  h.values
-  ing = Ingredient.new(name: h.values)
+JSON.parse(list.read)["drinks"].each do |drink|
+  drink.values
+  ing = Ingredient.new(name: drink.values)
   ing.save
 end
